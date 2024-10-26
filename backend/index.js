3df0a292cb14cd85f48623fb9a5e7c26c0a07f53
Shravan Joshi <shravanjoshihydi@gmail.com>
2024-10-26 18:15:08 +0530

    Update a Book

--- backend/index.js
+++ backend/index.js
@@ -68,6 +68,35 @@ app.get("/books/:id", async (request,response) => {
     }
 })
 
+// Route to update a book
+app.put("/books/:id", async (request,response) => {
+try{
+    if(
+        !request.body.title || 
+        !request.body.author ||
+        !request.body.publishYear
+    ){
+        return response.status(400).send({
+            message: 'Send all the required fields: title, author, publishYear',
+        });
+    }
+
+    const {id} = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if(!result) {
+        return response.status(404).json({message: 'Book not found'});
+    }
+
+    return response.status(200).json({messgae:"Book updated successfully"});
+
+}catch(error){
+    console.log(error);
+    response.status(500).send({message: error.message});
+}
+})
+
 //connect to database
 mongoose
     .connect(mongoDBURL)
