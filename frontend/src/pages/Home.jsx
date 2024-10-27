b84c515ba3418f350bc55b47cee61774b9c1b6b2
Shravan Joshi <shravanjoshihydi@gmail.com>
2024-10-27 15:11:06 +0530

    adding snackbar alerts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
