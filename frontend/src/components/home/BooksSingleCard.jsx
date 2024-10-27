6aad1c91a1f6b68ecc04bf139decf8566f5b57c4
Shravan Joshi <shravanjoshihydi@gmail.com>
2024-10-27 14:14:27 +0530

    creating book modal

--- frontend/src/components/home/BooksSingleCard.jsx
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
+import { BiUserCircle , BiShow} from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import { useState } from "react";
+import BookModal from "./BookModal";
 
 const BooksSingleCard = ({book}) => {
+    const [showModal, setShowModal] = useState(false);
   return (
     <div
           key={book._id}
@@ -25,6 +28,10 @@ const BooksSingleCard = ({book}) => {
             <h2 className="my-1">{book.author}</h2>
           </div>
           <div className="flex justify-between books-center gap-x-2 mt-4 p-4">
+            <BiShow 
+                className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+                onClick={()=> setShowModal(true)}
+            />
             <Link to={`/books/details/${book._id}`}>
               <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
             </Link>
@@ -35,6 +42,11 @@ const BooksSingleCard = ({book}) => {
               <MdOutlineDelete className="tex-2xl text-red-600 hover:text-black" />
             </Link>
           </div>
+          {
+            showModal && (
+                <BookModal book={book} onClose={() => setShowModal(false)}/>
+            )
+          }
         </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
 import BooksTable from "../components/home/BooksTable";
 import BooksCard from "../components/home/BooksCard";
 
