7a0a1ac99ef78b925eb8c0bd8cc6e2c3a1dcfcaa
Shravan Joshi <shravanjoshihydi@gmail.com>
2024-10-27 13:47:30 +0530

    books table and cards components

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import BooksSingleCard from "./BooksSingleCard";
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((book) => (
+        <BooksSingleCard key ={book._id} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
